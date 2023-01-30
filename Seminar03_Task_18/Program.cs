c18e389efb7d6c24f7162271b8a29e8e3449df19
Yuriy <databatonas@gmail.com>
2023-01-30 20:29:36 +0300

    Задача по нахождению диапозона координат по заданной четверти

---
+++ Seminar03_Task_18/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая по заданному номеру четверти, 
+   показывает диапозон возможных координат точек в этой четверти (X и Y).*/
+
+Console.WriteLine("Введите номер четверти от 1 до 4, используя целые значения");
+
+int quarter = Convert.ToInt32(Console.ReadLine());/* quarter- четверть*/
+
+/*Если четверть меньше 1 или больше 4*/
+if(quarter < 1 || quarter > 4) Console.WriteLine("Вы ввели некорректное число");
+
+if(quarter == 1) Console.WriteLine("X > 0  и Y > 0");
+if(quarter == 2) Console.WriteLine("X < 0  и Y > 0");
+if(quarter == 3) Console.WriteLine("X < 0  и Y < 0");
+if(quarter == 4) Console.WriteLine("X > 0  и Y < 0");
